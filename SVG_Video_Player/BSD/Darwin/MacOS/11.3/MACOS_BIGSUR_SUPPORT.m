98f8869102bfb401d18be8f250c232734faf6a60
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-25 11:16:04 -0700

    MACOS_BIGSUR_SUPPORT.m
    The MacOS 11.3 Big Sur support script for this project.

---
+++ SVG_Video_Player/BSD/Darwin/MacOS/11.3/MACOS_BIGSUR_SUPPORT.m
@@ -0,0 +1,23 @@
+@interface classname : MacOS11_BigSur_Support {
+  // Start of script
+  // instance variables
+}
+  + classMethod1;
+  + (return_type)classMethod2;
+  + (return_type)classMethod3:(param1_type)param1_varName;
+  - (return_type)instanceMethod1With1Parameter:(param1_type)param1_varName;
+  - (return_type)instanceMethod2With2Parameters:(param1_type)param1_varName
+param2_callName:(param2_type)param2_varName;
+  @MacOS_11_3_ObjectiveC classname : superclassname {
+  supportUID = str("MacOS 11.3 Big Sur x64"); // Operating system name and version
+  supportSYS = str("BSD"); // Base distribution name and version
+  supportKRN = str("DARWIN 20.4.0"); // Kernel name and version
+	NSLog(@"Mac OS 11.3 (Big Sur) support is coming soon");
+}
+@end
+/* File info
+* File version: `1 (Sunay, April 25th 2021 at 11:16 pm)
+* File type: Objective-C Source file (*.m)
+* Line count (including blank lines and compiler line): 24
+* This script is incomplete, and needs lots of work */
+// End of script
