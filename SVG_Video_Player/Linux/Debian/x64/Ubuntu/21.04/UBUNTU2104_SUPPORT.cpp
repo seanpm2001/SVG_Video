b088b8a812081fec6347cacf4d02ae16d3eca132
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-24 20:56:08 -0700

    UBUNTU2104_SUPPORT.cpp
    The support script for Ubuntu 21.04.

---
+++ SVG_Video_Player/Linux/Debian/x64/Ubuntu/21.04/UBUNTU2104_SUPPORT.cpp
@@ -0,0 +1,33 @@
+// Start of script
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+// Support script for Ubuntu 21.04
+// Please note that I am not experienced with C++ so this file may not be functional and may come off as bad code
+class Ubuntu2010_SVGVideoPlayer {
+  int merge() {
+    // Merge is currently empty
+    // File: /SVG_Video/Header/Header.xml | /SVG_Video/SubHeader/SubHeader.xml (optional)
+    }
+  int export() {
+    std::cout >> "Exporting video as *.svg_vid *.svg_video\n";
+    std::cout >> "Export coming soon\n";
+  }
+  int main() {
+    // get.stream(deb64) // Need better syntax that actually works
+    supportUID = str("Ubuntu 21.04 64 bit"); // Operating system name and version
+    supportSYS = str("Debian 9"); // Base distribution name and version
+    supportKRN = str("Linux 5.11"); // Kernel name and version
+    integerTypeBase = int(64);
+     // SVG video is an advanced video format, where video is recorded in SVG format. This will significantly reduce the size, but will be a very difficult programming task, and may use extra resources
+    merge();
+    std::cout << "Ubuntu 21.04 support is coming soon";
+    }
+// Divider between main and class
+}
+/* File info
+* File type: C++ Source file (*.cpp, *.cxx)
+* File version: 1 (Saturday, April 24th 2021 at 8:56 pm)
+* Line count (including blank lines and compiler line): 34
+*/
+// End of script
