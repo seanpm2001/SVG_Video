49bafb6570a5996f0bce5d6b20602cb3751638d3
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-04-27 15:16:33 -0700

    WIN10_2004_SUPPORT_V1.cs
    An archive of the first version of the Windows 10 build 2004 support script for this project.

---
+++ SVG_Video_Player/NT/Windows/Windows10/Build2004/x32/OldVersions/WIN10_2004_SUPPORT_V1.cs
@@ -0,0 +1,36 @@
+// Start of script
+// Support for Windows 10 build 2004 (x86, 32 bit)
+// Please note that I am not experienced with programming in C# so this script may not work. It is currently a demo script with bad programming methods.
+// Definitions
+class windows10_2004(2004) { // Trying to form a class here, so it can detect from different Windows 10 2004 version support files.
+kernelVersionInt = float(10.0);
+systemReleaseStr = str("2004");
+processorType = int(32);
+}
+// Main
+using System;
+class Program
+{
+  public method kernelVersion() {
+    return kernelVersionInt as char;
+  }
+  public method systemReleaseVersion() {
+    return systemReleaseStr as char;
+  }
+  public method processorTypeInfo() {
+    return processorType as char;
+  }
+    public static void Main(string[] args) {
+      return kernelVersion();
+      return systemReleaseVersion();
+      return processorType();
+}} else: {
+      quit(); // Exits the prompt
+} // End of main method
+/* File info
+* File version: `1 (Tuesday, April 27th 2021 at 3:16 pm)
+* File type: Microsoft C# Source file (*.cs, *.csx)
+* Line count (including blank lines and compiler line): 37
+*/
+// This script is incomplete, and needs lots of work
+// End of script
